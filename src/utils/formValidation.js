730d1941b00c5d0b37a7bd36b0919593f0596ac4
Elias Afara <afaraelias@gmail.com>
2022-07-26 00:00:49 +0200

    Added validation for election year

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,64 +1,66 @@
 export const addCandidateFormValidation = (setFormErrors, name, value) => {
-    setFormErrors((prev) => {
-        const errorStateObj = { ...prev, [name]: '' };
+  setFormErrors((prev) => {
+    const errorStateObj = { ...prev, [name]: '' };
 
-        switch (name) {
-            case 'citizen_ssn':
-                if (!(value.length === 15)) {
-                    errorStateObj[name] =
-                        'Social Security Number must be of 15 characters';
-                }
-                break;
-
-            case 'election_round':
-                if (!(value === '1st' || value === '2nd')) {
-                    errorStateObj[name] = 'Election Round value must be either 1st or 2nd';
-                }
-                break;
-
-            default:
-                break;
+    switch (name) {
+      case 'citizen_ssn':
+        if (!(value.length === 15)) {
+          errorStateObj[name] =
+            'Social Security Number must be of 15 characters';
         }
+        break;
 
-        return errorStateObj;
-    });
+      case 'election_round':
+        if (!(value === '1st' || value === '2nd')) {
+          errorStateObj[name] =
+            'Election Round value must be either 1st or 2nd';
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    return errorStateObj;
+  });
 };
 
 export const addCitizenFormValidation = (setFormErrors, name, value) => {
-    setFormErrors((prev) => {
-        const errorStateObj = { ...prev, [name]: '' };
+  setFormErrors((prev) => {
+    const errorStateObj = { ...prev, [name]: '' };
 
-        switch (name) {
-            case 'citizen_ssn':
-                if (!(value.length === 15)) {
-                    errorStateObj[name] =
-                        'Social Security Number must be of 15 characters';
-                }
-                break;
-
-            default:
-                break;
+    switch (name) {
+      case 'citizen_ssn':
+        if (!(value.length === 15)) {
+          errorStateObj[name] =
+            'Social Security Number must be of 15 characters';
         }
+        break;
 
-        return errorStateObj;
-    });
+      default:
+        break;
+    }
+
+    return errorStateObj;
+  });
 };
 
 export const addElectionFormValidation = (setFormErrors, name, value) => {
-    setFormErrors((prev) => {
-        const errorStateObj = { ...prev, [name]: '' };
+  setFormErrors((prev) => {
+    const errorStateObj = { ...prev, [name]: '' };
 
-        switch (name) {
-            case 'election_round':
-                if (!(value === '1st' || value === '2nd')) {
-                    errorStateObj[name] = 'Election Round value must be either 1st or 2nd';
-                }
-                break;
-
-            default:
-                break;
+    switch (name) {
+      case 'election_year':
+        if (!(value >= 2022)) {
+          errorStateObj[name] =
+            'Election Year must be greater than or equal to 2022';
         }
+        break;
 
-        return errorStateObj;
-    });
+      default:
+        break;
+    }
+
+    return errorStateObj;
+  });
 };
