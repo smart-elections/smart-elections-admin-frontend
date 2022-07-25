9fef82f9d111ccc25b9629768962dbef2797ace9
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-25 20:34:23 +0200

    view all candidates page with backend call

--- src/pages/candidates/AllCandidates.jsx
+++ src/pages/candidates/AllCandidates.jsx
@@ -2,47 +2,58 @@ import React from 'react';
 import { useState, useEffect } from 'react'
 import axios from "axios";
 import MUIDataTable from "mui-datatables";
-import { CircularProgress, Typography } from "@material-ui/core";
+import { Select, MenuItem } from "@material-ui/core";
 import './candidates.scss';
 
 const AllCandidates = () => {
 
   const [candidates, setCandidates] = useState([]);
 
-  const getCandidates = async () => {
-    const response = await fetch('http://ec2-34-207-166-28.compute-1.amazonaws.com:8000/candidates');
-    const data = await response.json();
-    setCandidates(data.data);
+  const options = {
+    filter: true,
+    filterType: 'dropdown',
+    responsive: 'stacked',
+    rowsPerPage: 10,
+    rowsPerPageOptions: [10, 25, 50, 100],
+    selectableRows: 'none',
+    print: false,
+    download: false,
+    viewColumns: false,
+  };
+
+  const baseUrl = "http://ec2-34-207-166-28.compute-1.amazonaws.com:8000/elections/candidates";
+
+  const getData = async () => {
+    const response = await axios.get(baseUrl);
+    setCandidates(response.data.data);
   }
 
   useEffect(() => {
-    getCandidates();
+    getData();
   }
-    , []);
+    , [])
+
+  const columns = [
+    { name: "election_id", label: "Election ID", options: { display: 'false', filter: false } },
+    { name: "election_year", label: "Election Year", options: { display: 'true', filter: false } },
+    { name: "election_type", label: "Election Type", options: { display: 'true', filter: false } },
+    { name: "election_round", label: "Election Round", options: { display: 'true', filter: false } },
+    { name: "candidate_id", label: "Candidate ID", options: { display: 'false', filter: false } },
+    { name: "citizen_firstname", label: "First Name", options: { display: 'true', filter: false } },
+    { name: "citizen_lastname", label: "Last Name", options: { display: 'true', filter: false } },
+    { name: "candidate_party", label: "Political Party", options: { display: 'true' } },
+    { name: "citizen_ssn", label: "Social Security Number", options: { display: 'true' } }
+  ]
 
   return (
-    <div className="candidates__container">
-      <h3 className="p-3 text-center">All Candidates</h3>
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Party</th>
-            <th>Citizen SSN</th>
-          </tr>
-        </thead>
-        <tbody>
-          {candidates && candidates.map((candidate) => (
-            <tr key={candidate.id}>
-              <td>{candidate.citizen_firstname}</td>
-              <td>{candidate.citizen_lastname}</td>
-              <td>{candidate.candidate_party}</td>
-              <td>{candidate.citizen_ssn}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <MUIDataTable
+        className='candidatesTable'
+        title={"All Candidates"}
+        columns={columns}
+        data={candidates}
+        options={options}
+      />
     </div>
   );
 };
