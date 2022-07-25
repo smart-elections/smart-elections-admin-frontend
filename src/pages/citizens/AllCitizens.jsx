2dc0019f1ea716c66922e61609a7b658d60e63a7
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-25 21:32:35 +0200

    view all citizens with backend connection

--- src/pages/citizens/AllCitizens.jsx
+++ src/pages/citizens/AllCitizens.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import MUIDataTable from "mui-datatables";
 import './citizens.scss';
 
 const AllCitizens = () => {
-  return <div>AllCitizens</div>;
+
+  const [citizens, setCitizens] = useState([]);
+
+  const options = {
+    filter: true,
+    filterType: 'dropdown',
+    responsive: 'stacked',
+    rowsPerPage: 10,
+    rowsPerPageOptions: [10, 25, 50, 100],
+    selectableRows: 'none',
+    print: false,
+    download: false,
+    viewColumns: false,
+  };
+
+  const baseUrl = "http://ec2-34-207-166-28.compute-1.amazonaws.com:8000/citizens";
+
+  const getData = async () => {
+    const response = await axios.get(baseUrl);
+    setCitizens(response.data.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }
+    , [])
+
+  const columns = [
+    { name: "citizen_id", label: "Citizen ID", options: { display: 'true', filter: false } },
+    { name: "citizen_ssn", label: "Social Security Number", options: { display: 'false', filter: false } },
+    { name: "citizen_firstname", label: "First Name", options: { display: 'true', filter: false } },
+    { name: "citizen_lastname", label: "Last Name", options: { display: 'true', filter: false } },
+    { name: "citizen_gender", label: "Gender", options: { display: 'true', filter: false } },
+    { name: "citizen_commune", label: "Commune", options: { display: 'false', filter: true } },
+
+  ]
+
+  return (
+    <div>
+      <MUIDataTable
+        className='citizenstable'
+        title={"All Citizens"}
+        columns={columns}
+        data={citizens}
+        options={options}
+      />
+    </div>
+  );
 };
 
 export default AllCitizens;
