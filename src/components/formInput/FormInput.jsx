bfefa1ce4a5c9fcc9fcc4f2ab46f6249ee1a947e
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-25 17:38:17 +0200

    added labels to add election form

--- src/components/formInput/FormInput.jsx
+++ src/components/formInput/FormInput.jsx
@@ -3,6 +3,7 @@ import './formInput.scss';
 const FormInput = ({ onChange, errorMessage, ...inputProps }) => {
   return (
     <div className='formInput'>
+      <label htmlFor={inputProps.name}>{inputProps.label}</label>
       <input {...inputProps} onChange={onChange} autoComplete='off' />
       <span className='err'>{errorMessage && errorMessage}</span>
     </div>
