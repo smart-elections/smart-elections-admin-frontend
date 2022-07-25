7e6fa4bbb6e32c6e1835a40fa2164b0343c54f18
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-25 18:57:51 +0200

    view candidates 1st draft

--- src/pages/candidates/AllCandidates.jsx
+++ src/pages/candidates/AllCandidates.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
+
 import './candidates.scss';
 
 const AllCandidates = () => {
-  return <div>AllCandidates</div>;
+
+  const [candidates, setCandidates] = useState([]);
+
+  const getCandidates = async () => {
+    const response = await fetch('http://ec2-34-207-166-28.compute-1.amazonaws.com:8000/candidates');
+    const data = await response.json();
+    setCandidates(data.data);
+  }
+
+  useEffect(() => {
+    getCandidates();
+  }
+    , []);
+
+  return (
+    <div className="candidates__container">
+      <h3 className="p-3 text-center">All Candidates</h3>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Party</th>
+            <th>Citizen SSN</th>
+          </tr>
+        </thead>
+        <tbody>
+          {candidates && candidates.map((candidate) => (
+            <tr key={candidate.id}>
+              <td>{candidate.citizen_firstname}</td>
+              <td>{candidate.citizen_lastname}</td>
+              <td>{candidate.candidate_party}</td>
+              <td>{candidate.citizen_ssn}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AllCandidates;
